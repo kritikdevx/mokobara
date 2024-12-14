850174ed313e13c904cbca444e548378615f11d9
kritikdevx <kritik.jiyaviya@devxconsultancy.com>
2024-12-14 15:11:38 +0530

    update validaton message

--- libs/validator.ts
+++ libs/validator.ts
@@ -1,20 +1,36 @@
 import { z } from "zod";
 
 export const warrantyClaimSchema = z.object({
-  full_name: z.string().min(1, { message: "Full name is required" }),
-  email: z.string().email({ message: "Invalid email address" }),
+  full_name: z
+    .string({ message: "Full name is required" })
+    .min(1, { message: "Full name is required" }),
+  email: z
+    .string({ message: "Email is required" })
+    .email({ message: "Invalid email address" }),
   phone: z
-    .string()
+    .string({ message: "Phone number is required" })
     .regex(/^\+?\d{10,15}$/, { message: "Invalid phone number" }),
-  address: z.string().min(1, { message: "Address is required" }),
-  order_number: z.string().min(1, { message: "Order number is required" }),
-  order_date: z.string().refine((val) => !isNaN(Date.parse(val)), {
-    message: "Invalid order date format",
-  }),
-  product_type: z.string().min(1, { message: "Product type is required" }),
-  product_name: z.string().min(1, { message: "Product name is required" }),
+  address: z
+    .string({ message: "Address is required" })
+    .min(1, { message: "Address is required" }),
+  order_number: z
+    .string({ message: "Order number is required" })
+    .min(1, { message: "Order number is required" }),
+  order_date: z
+    .string({ message: "Order date is required" })
+    .refine((val) => !isNaN(Date.parse(val)), {
+      message: "Invalid order date format",
+    }),
+  product_type: z
+    .string({ message: "Product type is required" })
+    .min(1, { message: "Product type is required" }),
+  product_name: z
+    .string({ message: "Product name is required" })
+    .min(1, { message: "Product name is required" }),
   description: z.string().trim().optional(),
-  invoice: z.string().min(1, { message: "Invoice is required" }),
+  invoice: z
+    .string({ message: "Invoice is required" })
+    .min(1, { message: "Invoice is required" }),
   images: z
     .array(z.string())
     .min(1, { message: "Images are required" })
